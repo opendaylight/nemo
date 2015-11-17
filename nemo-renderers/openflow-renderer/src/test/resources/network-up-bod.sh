32e662b56aa9f9528564a195f5b82cf8e012e3aa
Zhigang Ji <jizhigang@huawei.com>
2015-11-17 16:43:21 +0800

    Modify the shell script for create physical topology for BoD scenario.
    Change-Id: If2b21667112fb1526671222cb25dc9db63637abb
    Signed-off-by: Zhigang Ji <jizhigang@huawei.com>

--- nemo-renderers/openflow-renderer/src/test/resources/network-up-bod.sh
+++ nemo-renderers/openflow-renderer/src/test/resources/network-up-bod.sh
@@ -45,14 +45,6 @@ ofprotocol unix:/tmp/sw3 tcp:127.0.0.1:6633 --fail=closed --listen=ptcp:6663 1> 
 ofdatapath -i sw4-eth1,eth3 punix:/tmp/sw4 -d 000000000004 1> /tmp/sw4-ofd.log 2> /tmp/sw4-ofd.log &
 ofprotocol unix:/tmp/sw4 tcp:127.0.0.1:6633 --fail=closed --listen=ptcp:6664 1> /tmp/sw4-ofp.log 2> /tmp/sw4-ofp.log &
 
-sleep 5
-
-########## lldp ##########
-dpctl unix:/tmp/sw1 flow-mod cmd=add,table=0 eth_type=0x88cc apply:output=ctrl:0xff
-dpctl unix:/tmp/sw2 flow-mod cmd=add,table=0 eth_type=0x88cc apply:output=ctrl:0xff
-dpctl unix:/tmp/sw3 flow-mod cmd=add,table=0 eth_type=0x88cc apply:output=ctrl:0xff
-dpctl unix:/tmp/sw4 flow-mod cmd=add,table=0 eth_type=0x88cc apply:output=ctrl:0xff
-
 exit 0
 
 
@@ -65,7 +57,6 @@ ifconfig server1-eth0 up
 ifconfig server1-eth0
 route add default gw 192.168.11.1
 route -n
-arp -s 192.168.11.1 00:00:0a:0b:0c:01
 exit
 
 ip netns exec vm1 bash
@@ -76,9 +67,6 @@ ifconfig vm1-eth0 up
 ifconfig vm1-eth0
 route add default gw 192.168.12.1
 route -n
-arp -s 192.168.12.1 00:00:0a:0b:0c:02
-arp -s 192.168.12.3 00:00:00:00:00:02
-arp -s 192.168.12.4 08:00:27:a0:a9:b3
 exit
 
 ip netns exec vm2 bash
@@ -89,9 +77,6 @@ ifconfig vm2-eth0 up
 ifconfig vm2-eth0
 route add default gw 192.168.12.1
 route -n
-arp -s 192.168.12.1 00:00:0a:0b:0c:02
-arp -s 192.168.12.2 00:00:00:00:00:01
-arp -s 192.168.12.4 08:00:27:a0:a9:b3
 exit
 
 
