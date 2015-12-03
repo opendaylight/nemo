57c4775561e31453dac5dd679e1767ee971713b7
zhongwei <zhongwei_xdu@outlook.com>
2015-12-03 17:59:31 +0800

    Modify nemo-impl updateintent test files
    Change-Id: I3021f0f54e11e4b59d3b1e6276a740d42e2c5a21
    Signed-off-by: zhongwei <zhongwei_xdu@outlook.com>

--- nemo-impl/src/test/java/org/opendaylight/nemo/user/vnspacemanager/structurestyle/updateintent/UpdateFlowTest.java
+++ nemo-impl/src/test/java/org/opendaylight/nemo/user/vnspacemanager/structurestyle/updateintent/UpdateFlowTest.java
@@ -6,15 +6,37 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.opendaylight.nemo.user.vnspacemanager.structurestyle.updateintent;
-import static org.mockito.Mockito.mock;
+import org.opendaylight.nemo.user.vnspacemanager.structurestyle.updateintent.UpdateFlow;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.nemo.user.tenantmanager.TenantManage;
 import org.opendaylight.nemo.user.vnspacemanager.instancecheck.FlowInstanceCheck;
 import org.opendaylight.nemo.user.vnspacemanager.syntaxcheck.FlowDefinitionCheck;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.common.rev151010.UserId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.Users;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.Objects;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.objects.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.objects.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.objects.FlowKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.users.User;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.users.UserKey;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by ldzd11 on 2015/11/9.
@@ -25,28 +47,48 @@ public class UpdateFlowTest {
 
     private DataBroker dataBroker;
     private TenantManage tenantManage;
-    private FlowDefinitionCheck flowDefinitionCheck;
-    private FlowInstanceCheck flowInstanceCheck;
     private UserId userId;
     private Flow flow;
+    
     @org.junit.Before
     public void setUp() throws Exception {
         dataBroker = mock(DataBroker.class);
         tenantManage = mock(TenantManage.class);
-        flowDefinitionCheck = mock(FlowDefinitionCheck.class);
-        flowInstanceCheck = mock(FlowInstanceCheck.class);
         userId = mock(UserId.class);
         flow = mock(Flow.class);
-
-
-        updateFlow = mock(UpdateFlow.class);
+        
+        updateFlow = new UpdateFlow(dataBroker, tenantManage);
+        //updateFlow = mock(UpdateFlow.class);
 
     }
 
     @org.junit.Test
     public void testFlowHandling() throws Exception {
-
-        Assert.assertNull(updateFlow.FlowHandling(userId, flow));
+    	
+    	String errorDefinition = new String("The match item has not been defined.");
+    	
+    	CheckedFuture connectiondefinitionFuture = mock(CheckedFuture.class);
+        ReadOnlyTransaction readOnlyTransaction = mock(ReadOnlyTransaction.class);
+//       when(dataBroker.newReadOnlyTransaction().read(any(LogicalDatastoreType.class),
+//       any(InstanceIdentifier.class))).thenReturn(userRolesFuture);
+        when(dataBroker.newReadOnlyTransaction()).thenReturn(readOnlyTransaction);
+        when(readOnlyTransaction.read(any(LogicalDatastoreType.class), any(InstanceIdentifier.class))).thenReturn(connectiondefinitionFuture); 
+         
+    	//when(flowDefinitionCheck.CheckDefinition(any(Flow.class))).thenReturn(errorDefinition);
+    	Assert.assertEquals(updateFlow.FlowHandling(userId,flow),errorDefinition);
+    	
+    	//errorDefinition = null;
+    	//String errorInstance = new String("Case 1");
+    	//when(flowInstanceCheck.checkFlowInstance(any(UserId.class),any(Flow.class))).thenReturn(errorInstance);
+    	//Assert.assertEquals(updateFlow.FlowHandling(userId,flow),errorInstance);
+    	
+    	//errorInstance = null;
+    	//userId = null;
+    	//flow = null;
+    	//WriteTransaction t = mock(WriteTransaction.class);
+    	//when(dataBroker.newWriteOnlyTransaction()).thenReturn(t);
+    	
+        //Assert.assertNull(updateFlow.FlowHandling(userId, flow));
 
     }
 }
--- nemo-impl/src/test/java/org/opendaylight/nemo/user/vnspacemanager/structurestyle/updateintent/UpdateResultTest.java
+++ nemo-impl/src/test/java/org/opendaylight/nemo/user/vnspacemanager/structurestyle/updateintent/UpdateResultTest.java
@@ -6,20 +6,48 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.opendaylight.nemo.user.vnspacemanager.structurestyle.updateintent;
-import static org.mockito.Mockito.mock;
+import org.opendaylight.nemo.user.vnspacemanager.structurestyle.updateintent.UpdateResult;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.nemo.user.tenantmanager.TenantManage;
+import org.opendaylight.nemo.user.vnspacemanager.instancecheck.OperationInstanceCheck;
+import org.opendaylight.nemo.user.vnspacemanager.syntaxcheck.OperationDefinitionCheck;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.common.rev151010.UserId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.Users;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.Operations;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.operations.Operation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.operations.OperationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.operations.OperationKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.users.User;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.users.UserKey;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import com.google.common.util.concurrent.CheckedFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.nemo.user.tenantmanager.TenantManage;
 import org.opendaylight.nemo.user.vnspacemanager.instancecheck.ResultInstanceCheck;
 import org.opendaylight.nemo.user.vnspacemanager.syntaxcheck.ResultDefinitionCheck;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.common.rev151010.UserId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.intent.rev151010.user.intent.Results;
+
+
 /**
  * Created by ldzd11 on 2015/11/9.
  */
 public class UpdateResultTest {
-
 
     private UpdateResult updateResult;
 
@@ -29,7 +57,7 @@ public class UpdateResultTest {
     private ResultInstanceCheck resultInstanceCheck;
     private UserId userId;
     private Results results;
-
+    
     @org.junit.Before
     public void setUp() throws Exception {
         dataBroker = mock(DataBroker.class);
@@ -38,14 +66,30 @@ public class UpdateResultTest {
         resultInstanceCheck = mock(ResultInstanceCheck.class);
         userId = mock(UserId.class);
         results = mock(Results.class);
-
-        updateResult = mock(UpdateResult.class);
-
+        
+        updateResult = new UpdateResult(dataBroker, tenantManage);
+        //updateFlow = mock(UpdateFlow.class);
 
     }
 
     @org.junit.Test
     public void testResultHandling() throws Exception {
+    	
+    	String errorDefinition = null;
+    	when(resultDefinitionCheck.CheckDefinition(any(Results.class))).thenReturn(null);
+    	Assert.assertEquals(updateResult.ResultHandling(userId,results),errorDefinition);
+    	
+    	errorDefinition = null;
+    	String errorInstance = null;
+    	when(resultInstanceCheck.checkResultInstance(any(UserId.class),any(Results.class))).thenReturn(errorInstance);
+    	Assert.assertEquals(updateResult.ResultHandling(userId,results),errorInstance);
+    	
+    	errorInstance = null;
+    	//userId = null;
+    	//results = null;
+    	//WriteTransaction t = mock(WriteTransaction.class);
+    	//when(dataBroker.newWriteOnlyTransaction()).thenReturn(t);
+    	
         Assert.assertNull(updateResult.ResultHandling(userId, results));
 
     }
