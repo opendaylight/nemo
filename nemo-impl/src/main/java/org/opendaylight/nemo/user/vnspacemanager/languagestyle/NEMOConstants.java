834f0a931dad36d15c6dcca70c7905a3b1e842b7
Zhang Yali <zhangyali369@huawei.com>
2015-12-04 18:34:57 +0800

    Add nemo language parser implementation.
    Change-Id: Ia5f2df2d8d469ed591a026612402b11fc72b3df3
    Signed-off-by: Zhang Yali <zhangyali369@huawei.com>

---
+++ nemo-impl/src/main/java/org/opendaylight/nemo/user/vnspacemanager/languagestyle/NEMOConstants.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2015 Huawei, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.nemo.user.vnspacemanager.languagestyle;
+
+/**
+ * Created by z00293636 on 2015/11/25.
+ */
+public interface NEMOConstants {
+    String range = "range";
+    String string = "string";
+    String integer = "int";
+    String ethaddr = "ethaddr";
+    String ipv4pref = "ipv4pref";
+    String ipv4addr = "ipv4addr";
+    String mac_address = "mac-address";
+    String ip_address = "ip-address";
+    String ip_prefix = "ip-prefix";
+    String gateway_ip = "gateway-ip";
+    String ac_info_network = "ac-info-network";
+    String operating_mode = "operating-mode";
+    String layer2 = "layer2";
+    String layer3 = "layer3";
+    String host = "host";
+    String src_mac = "src-mac";
+    String dst_mac = "dst-mac";
+    String src_ip = "src-ip";
+    String dst_ip = "dst_ip";
+    String colon = ":";
+    String not = "!";
+    String and = "&&";
+    String or = "||";
+    String lparen = "(";
+    String rparen = ")";
+    String lbracket = "[";
+    String rbracket = "]";
+    String equal = "==";
+    String not_equal = "!=";
+    String greater_than = ">";
+    String less_than = "<";
+    String not_greater_than = "<=";
+    String not_less_than = ">=";
+    String between = "*";
+    String comma = ",";
+    String time = "time";
+    String node = "Node";
+    String connection = "Connection";
+    String flow = "Flow";
+    String operation = "Operation";
+    String AllNodes = "nodes";
+    String AllConnections = "connections";
+    String AllFlows = "flows";
+    String AllOperations = "operations";
+    String Type = "Type";
+    String Contain = "Contain";
+    String SubNodes = "Subnodes";
+    String Property = "Property";
+    String Endnodes = "Endnodes";
+    String Match = "Match";
+    String Priority = "Priority";
+    String Target = "Target";
+    String Condition = "Condition";
+    String Action = "Action";
+    String Id = "Id";
+    String Name = "Name";
+    String Results = "Results";
+    String create = "CREATE";
+    String delete = "DELETE";
+    String update = "UPDATE";
+    String Import = "IMPORT";
+    String query = "query";
+    String from = "From";
+    String transaction = "Transaction";
+    String begin = "Begin";
+    String end = "End";
+}
