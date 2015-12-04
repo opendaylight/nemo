30453f7f1500479950ac22101ccaa0b33c62e5d8
saomenmen <zhangmroy@163.com>
2015-12-04 11:53:07 +0800

    Modify nemo-impl and nemo-renderers partical test files
    Change-Id: Ib47b2dcf93c6c9fac49a94f53204324fad7d5154
    Signed-off-by: saomenmen <zhangmroy@163.com>

--- nemo-impl/src/test/java/org/opendaylight/nemo/intent/computation/VNComputationUnitTest.java
+++ nemo-impl/src/test/java/org/opendaylight/nemo/intent/computation/VNComputationUnitTest.java
@@ -7,58 +7,238 @@
  */
 package org.opendaylight.nemo.intent.computation;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import junit.framework.TestCase;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import com.google.common.base.Optional;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.nemo.intent.algorithm.Edge;
+import org.opendaylight.nemo.intent.algorithm.RoutingAlgorithm;
+import org.opendaylight.nemo.intent.algorithm.Vertex;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.VirtualNetworks;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.VirtualNetwork;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.VirtualNetworkKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.VirtualLinks;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.VirtualNodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.VirtualPaths;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.VirtualRoutes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.virtual.links.VirtualLink;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.virtual.nodes.VirtualNode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.virtual.paths.VirtualPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.virtual.paths.VirtualPathBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.virtual.paths.VirtualPathKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.virtual.routes.VirtualRoute;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.virtual.routes.VirtualRouteBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.networks.virtual.network.virtual.routes.VirtualRouteKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.virtual.path.instance.VirtualLinkBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.common.rev151010.UserId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.engine.common.rev151010.VirtualLinkId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.engine.common.rev151010.VirtualNetworkId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.engine.common.rev151010.VirtualNodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.nemo.engine.common.rev151010.VirtualPathId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.generic.virtual.network.rev151010.VirtualNodeInstance.NodeType;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 /**
- * Created by zhangmeng on 2015/11/9.
+ * Created by zhangmeng on 2015/12/3.
  */
 public class VNComputationUnitTest extends TestCase {
-
     private VNComputationUnit vnComputationUnit;
     private DataBroker dataBroker;
-    private UserId userId;
-    private VirtualNodeId source,target;
-    private VirtualNodeId source1,target1;
-    private long bandwidth;
+    private VirtualNetwork virtualNetwork;
+    private VirtualNode virtualNode;
+    private VirtualNode virtualNode1;
+    private VirtualNode virtualNode2;
+    private VirtualNodeId nodeId;
+    private VirtualNodeId nodeId1;
+    private VirtualNodeId nodeId2;
+
     @Before
     public void setUp() throws Exception {
+        dataBroker = mock(DataBroker.class);
+        virtualNetwork = mock(VirtualNetwork.class);
+        UserId userId = mock(UserId.class);
+         virtualNode = mock(VirtualNode.class);
+         virtualNode1 = mock(VirtualNode.class);
+         virtualNode2 = mock(VirtualNode.class);
+        VirtualNodes virtualNodes = mock(VirtualNodes.class);
+        nodeId = mock(VirtualNodeId.class);
+        nodeId1 = mock(VirtualNodeId.class);
+        nodeId2= mock(VirtualNodeId.class);
+        NodeType nodeType = VirtualNode.NodeType.Vrouter;
+        VirtualLink virtualLink = mock(VirtualLink.class);
+        VirtualLinks virtualLinks = mock(VirtualLinks.class);
+        VirtualLinkId linkId = mock(VirtualLinkId.class);
+        VirtualNodeId srcVirtualNodeId = mock(VirtualNodeId.class);
+        VirtualNodeId destVirtualNodeId = mock(VirtualNodeId.class);
+        List<VirtualNode> virtualNodes_list  = new ArrayList<VirtualNode>();
+        List<VirtualLink> virtualLinks_list = new ArrayList<VirtualLink>();
 
-        source = mock(VirtualNodeId.class);
-        target = mock(VirtualNodeId.class);
-        source1 = mock(VirtualNodeId.class);
-        target1 = mock(VirtualNodeId.class);
-        vnComputationUnit = mock(VNComputationUnit.class);
+        virtualNodes_list.add(virtualNode);
+        virtualNodes_list.add(virtualNode1);
+        virtualNodes_list.add(virtualNode2);
+        virtualLinks_list.add(virtualLink);
+
+        when(virtualNetwork.getUserId()).thenReturn(userId);
+        when(virtualNetwork.getVirtualNodes()).thenReturn(virtualNodes);
+        when(virtualNodes.getVirtualNode()).thenReturn(virtualNodes_list);
+        when(virtualNode.getNodeId()).thenReturn(nodeId);
+        when(virtualNode1.getNodeId()).thenReturn(nodeId1);
+        when(virtualNode2.getNodeId()).thenReturn(nodeId2);
+        when(nodeId.getValue()).thenReturn(new String("src"));
+        when(nodeId1.getValue()).thenReturn(new String("dst"));
+        when(nodeId2.getValue()).thenReturn(new String("null"));
+
+        when(virtualNode.getNodeType())
+                .thenReturn(null)
+                .thenReturn(nodeType);
+        when(virtualNode1.getNodeType())
+                .thenReturn(null)
+                .thenReturn(nodeType);
+        when(virtualNode2.getNodeType())
+                .thenReturn(null)
+                .thenReturn(nodeType);
+        when(virtualNetwork.getVirtualLinks()).thenReturn(virtualLinks);
+        when(virtualLinks.getVirtualLink()).thenReturn(virtualLinks_list);
+
+        when(virtualLink.getLinkId()).thenReturn(linkId);
+        when(linkId.getValue()).thenReturn(new String("00001111-0000-0000-0000-000011112222"));
+
+        when(virtualLink.getSrcNodeId()).thenReturn(srcVirtualNodeId);
+        when(srcVirtualNodeId.getValue()).thenReturn(new String("src"));
+
+        when(virtualLink.getDestNodeId()).thenReturn(destVirtualNodeId);
+        when(destVirtualNodeId.getValue()).thenReturn(new String("dst"));
+
+        when(virtualLink.getMetric()).thenReturn((long)1);
+
+        when(virtualLink.getBandwidth()).thenReturn((long)1);
+
+        vnComputationUnit = new VNComputationUnit(dataBroker,virtualNetwork);
+        vnComputationUnit = new VNComputationUnit(dataBroker,virtualNetwork);
+
+        verify(virtualNetwork,times(2)).getUserId();
+        verify(virtualNetwork,times(2)).getVirtualNodes();
+        verify(virtualNodes,times(2)).getVirtualNode();
+        verify(virtualNode,times(3)).getNodeId();
+        verify(virtualNode1, times(3)).getNodeId();
+        verify(virtualNode2, times(3)).getNodeId();
+        verify(nodeId, times(2)).getValue();
+        verify(nodeId1, times(2)).getValue();
+        verify(nodeId2, times(2)).getValue();
+        verify(virtualNode, times(2)).getNodeType();
+        verify(virtualNode1, times(2)).getNodeType();
+        verify(virtualNetwork,times(2)).getVirtualLinks();
+        verify(virtualLinks,times(2)).getVirtualLink();
+        verify(virtualLink,times(2)).getBandwidth();
+
     }
 
     @Test
     public void testComputePath() throws Exception {
-        vnComputationUnit.computePath(source, target);
-        verify(vnComputationUnit).computePath(source, target);
-        Assert.assertNotEquals(mock(VirtualPath.class), vnComputationUnit.computePath(source, target));
-        Assert.assertNotNull(vnComputationUnit);
+        VirtualNodeId source = mock(VirtualNodeId.class);
+        VirtualNodeId target = mock(VirtualNodeId.class);
+        VirtualPath result;
+
+        when(source.getValue()).thenReturn(new String("src"));
+        when(target.getValue())
+                .thenReturn(new String("null"))
+                .thenReturn(new String("dst"));
+
+        result = vnComputationUnit.computePath(source,target);
+        Assert.assertTrue(result == null);
+
+        result = vnComputationUnit.computePath(source,target);
+        Assert.assertTrue(result != null);
+
+        verify(source, times(2)).getValue();
+        verify(target,times(2)).getValue();
+
     }
 
     @Test
     public void testComputePath1() throws Exception {
-        vnComputationUnit.computePath(source1, target1, bandwidth);
-        verify(vnComputationUnit).computePath(source1, target1, bandwidth);
-        Assert.assertNotEquals(mock(VirtualPath.class), vnComputationUnit.computePath(source1, target1, bandwidth));
-        Assert.assertNotNull(vnComputationUnit);
+        VirtualNodeId source = mock(VirtualNodeId.class);
+        VirtualNodeId target = mock(VirtualNodeId.class);
+        VirtualPath result;
+
+        when(source.getValue()).thenReturn(new String("src"));
+        when(target.getValue())
+                .thenReturn(new String("null"))
+                .thenReturn(new String("dst"));
+
+        result = vnComputationUnit.computePath(source,target,1);
+        Assert.assertTrue(result == null);
+
+        result = vnComputationUnit.computePath(source,target,1);
+        Assert.assertTrue(result != null);
+
+        verify(source,times(2)).getValue();
+        verify(target,times(2)).getValue();
+
     }
 
     @Test
     public void testClose() throws Exception {
         vnComputationUnit.close();
-        Assert.assertNotNull(vnComputationUnit);
-        verify(vnComputationUnit).close();
+        Assert.assertTrue(vnComputationUnit != null);
+    }
+
+    @Test
+    public void testPrivateComputeRoute_withArgs() throws Exception{
+        VirtualNetwork virtualNetwork = mock(VirtualNetwork.class);
+        VirtualRoutes virtualRoutes = mock(VirtualRoutes.class);
+        VirtualRoute virtualRoute = mock(VirtualRoute.class);
+        VirtualPaths virtualPaths = mock(VirtualPaths.class);
+        VirtualPath virtualPath = mock(VirtualPath.class);
+        List<VirtualPath> virtualPathList = new ArrayList<VirtualPath>();
+        List<VirtualRoute> virtualRouteList = new ArrayList<VirtualRoute>();
+
+
+        Class<VNComputationUnit> class1 = VNComputationUnit.class;
+        Method method = class1.getDeclaredMethod("computeRoute",new Class[]{VirtualNetwork.class});
+        method.setAccessible(true);
+
+        when(virtualNetwork.getVirtualRoutes()).thenReturn(virtualRoutes);
+        when(virtualRoutes.getVirtualRoute()).thenReturn(virtualRouteList);
+        when(virtualNetwork.getVirtualPaths()).thenReturn(virtualPaths);
+        when(virtualPaths.getVirtualPath()).thenReturn(virtualPathList);
+
+        method.invoke(vnComputationUnit, virtualNetwork);
+
+        Assert.assertTrue(nodeId.equals(nodeId));
+        verify(virtualNetwork).getVirtualRoutes();
+        verify(virtualNetwork).getVirtualPaths();
+        verify(virtualPaths).getVirtualPath();
+        verify(virtualRoutes).getVirtualRoute();
+    }
+
+    @Test
+    public void testPrivateComputeRoute_withoutArgs() throws Exception{
+        ReadWriteTransaction readWriteTransaction = mock(ReadWriteTransaction.class);
+
+        Class<VNComputationUnit> class1 = VNComputationUnit.class;
+        Method method = class1.getDeclaredMethod("computeRoute");
+        method.setAccessible(true);
+
+        when(dataBroker.newReadWriteTransaction()).thenReturn(readWriteTransaction);
+
+
+        //method.invoke(vnComputationUnit);
     }
 }
--- nemo-renderers/openflow-renderer/src/test/java/org/opendaylight/nemo/renderer/openflow/physicalnetwork/PhyConfigLoaderTest.java
+++ nemo-renderers/openflow-renderer/src/test/java/org/opendaylight/nemo/renderer/openflow/physicalnetwork/PhyConfigLoaderTest.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.nemo.renderer.openflow.physicalnetwork;
+package org.opendaylight.nemo.renderer.openflow.phtysicalnetwork;
 
 import junit.framework.TestCase;
 import org.junit.Assert;
@@ -116,4 +116,225 @@ public class PhyConfigLoaderTest extends TestCase {
     public void testClose() throws Exception {
         phyConfigLoader.close();
     }
+
+    @Test
+    public void testBuildExternals() throws Exception{
+        JsonNode externalRoot = mock(JsonNode.class);
+        JsonNode exNetworkNodes = mock(JsonNode.class);
+        JsonNode exNetworkNode = mock(JsonNode.class);
+        JsonNode jsonNode = mock(JsonNode.class);
+        String nodeId = new String("1");
+        String portId = new String("2");
+        String peerMac = new String("00:11:22:33:44:55");
+
+        Class<PhyConfigLoader> class1 = PhyConfigLoader.class;
+        Method method = class1.getDeclaredMethod("buildExternals",new Class[]{JsonNode.class});
+        method.setAccessible(true);
+
+        when(externalRoot.path(any(String.class))).thenReturn(exNetworkNodes);
+        when(exNetworkNodes.size()).thenReturn(1);
+        when(exNetworkNodes.get(any(Integer.class))).thenReturn(exNetworkNode);
+
+        //get into method "buildExNetwork"
+        when(exNetworkNode.get(any(String.class))).thenReturn(jsonNode);
+        when(jsonNode.asText())
+                .thenReturn(nodeId)
+                .thenReturn(portId)
+                .thenReturn(peerMac);
+
+        method.invoke(phyConfigLoader, externalRoot);
+
+        verify(externalRoot).path(any(String.class));
+        verify(exNetworkNodes,times(2)).size();
+        verify(exNetworkNodes).get(any(Integer.class));
+        verify(exNetworkNode,times(3)).get(any(String.class));
+        verify(jsonNode,times(3)).asText();
+    }
+
+
+    @Test
+    public PhysicalLink testBuildLink(JsonNode arg)throws Exception{
+        if(arg == null)return null;
+
+        PhysicalLink physicalLink = mock(PhysicalLink.class);
+        JsonNode jsonNode = mock(JsonNode.class);
+        JsonNode jsonNode1 = mock(JsonNode.class);
+
+
+        Class<PhyConfigLoader> class1 = PhyConfigLoader.class;
+        Method method = class1.getDeclaredMethod("buildLink",new Class[]{JsonNode.class});
+        method.setAccessible(true);
+
+        when(jsonNode.get(any(String.class)))
+                .thenReturn(jsonNode1)
+                .thenReturn(jsonNode1);
+        when(jsonNode1.asText()).thenReturn(new String("test"));
+        when(jsonNode1.asLong()).thenReturn((long) 1);
+
+        physicalLink = (PhysicalLink)method.invoke(phyConfigLoader,jsonNode);
+        Assert.assertTrue(physicalLink != null);
+        Assert.assertTrue(physicalLink != mock(PhysicalLink.class));
+
+        return physicalLink;
+    }
+
+    @Test
+    public void testBuildLinks()throws Exception{
+        List<PhysicalLink> list;
+        JsonNode hostsNode = mock(JsonNode.class);
+        JsonNode hosts = mock(JsonNode.class);
+        JsonNode jsonNode = mock(JsonNode.class);
+        JsonNode jsonNode1 = mock(JsonNode.class);
+        PhysicalHost physicalHost;
+
+        Class<PhyConfigLoader> class1 = PhyConfigLoader.class;
+        Method method = class1.getDeclaredMethod("buildLinks",new Class[]{JsonNode.class});
+        method.setAccessible(true);
+
+        when(hostsNode.path(any(String.class))).thenReturn(hosts);
+        when(hosts.size()).thenReturn(1);
+        when(hosts.get(any(Integer.class))).thenReturn(jsonNode);
+        when(jsonNode.get(any(String.class)))
+                .thenReturn(jsonNode1)
+                .thenReturn(jsonNode1);
+        when(jsonNode1.asText()).thenReturn(new String("test"));
+        when(jsonNode1.asLong()).thenReturn((long) 1);
+
+        list = (List<PhysicalLink>)method.invoke(phyConfigLoader,hostsNode);
+        Assert.assertTrue(list.size() == 1);
+    }
+
+    @Test
+    public void testBuildHosts()throws Exception{
+        Class<PhyConfigLoader> class1 = PhyConfigLoader.class;
+        Method method = class1.getDeclaredMethod("buildHosts", new Class[]{JsonNode.class});
+        method.setAccessible(true);
+
+        JsonNode hostsNode = mock(JsonNode.class);
+        JsonNode hosts = mock(JsonNode.class);
+        JsonNode host = mock(JsonNode.class);
+        JsonNode host_temp = mock(JsonNode.class);
+        JsonNode host_temp1 = mock(JsonNode.class);
+        JsonNode ipaddrs = mock(JsonNode.class);
+        JsonNode ipaddr = mock(JsonNode.class);
+        List<PhysicalHost> list;
+
+        when(hostsNode.path(any(String.class))).thenReturn(hosts);
+        when(hosts.size()).thenReturn(1);
+        when(hosts.get(any(Integer.class))).thenReturn(host);
+        //get into method "buildhost"
+        when(host.get(any(String.class))).thenReturn(host_temp);
+        when(host_temp.asText())
+                .thenReturn(new String("00001111-0000-0000-0000-000011112222")) //HOST_ID
+                .thenReturn(new String("hostName")) //HOST_NAME
+                .thenReturn(new String("00:11:22:33:44:55")) //MAC_ADDRESS
+                .thenReturn(new String("nodeId")) //NODE_ID
+                .thenReturn(new String("connetionId"));//PhysicalPortId
+        when(host.path(any(String.class))).thenReturn(ipaddrs);
+        when(ipaddrs.size()).thenReturn(1);
+        when(ipaddrs.get(any(Integer.class))).thenReturn(ipaddr);
+        when(ipaddr.get(any(String.class))).thenReturn(host_temp1);
+        when(host_temp1.asText()).thenReturn(new String("192.168.1.1"));//ipv4_address
+
+
+        list = (List<PhysicalHost>) method.invoke(phyConfigLoader,hostsNode);
+        Assert.assertTrue(list.size() == 1);
+    }
+
+    public void testBuildPortAttributes()throws Exception{
+        Class<PhyConfigLoader>class1 = PhyConfigLoader.class;
+        Method method = class1.getDeclaredMethod("buildPortAttributes",new Class[]{JsonNode.class});
+        method.setAccessible(true);
+
+        List<Attribute> attributeList;
+        JsonNode attributes = mock(JsonNode.class);
+        JsonNode portAttribute = mock(JsonNode.class);
+        JsonNode jsonNode_temp = mock(JsonNode.class);
+
+        when(attributes.size()).thenReturn(1);
+        when(attributes.get(any(Integer.class))).thenReturn(portAttribute);
+        //get into method "buildPortAttribute"
+        when(portAttribute.path(any(String.class))).thenReturn(jsonNode_temp);
+        when(jsonNode_temp.asText())
+                .thenReturn(new String(""))//branch null
+                .thenReturn(new String("zm"));
+        attributeList = (List<Attribute>)method.invoke(phyConfigLoader,attributes);
+        Assert.assertTrue(attributeList.size() == 0);
+        //new AttributeName("zm");
+        attributeList = (List<Attribute>)method.invoke(phyConfigLoader,attributes);
+        Assert.assertTrue(attributeList.size() == 1);
+        verify(portAttribute,times(3)).path(any(String.class));
+        verify(jsonNode_temp,times(3)).asText();
+    }
+
+    @Test
+    public void testBuildNodes() throws Exception{
+        Class<PhyConfigLoader>class1 = PhyConfigLoader.class;
+        Method method = class1.getDeclaredMethod("buildNodes", new Class[]{JsonNode.class});
+        method.setAccessible(true);
+
+        JsonNode nodesRoot = mock(JsonNode.class);
+        JsonNode nodes = mock(JsonNode.class);
+        JsonNode node = mock(JsonNode.class);
+        JsonNode node_temp = mock(JsonNode.class);
+        JsonNode port_temp = mock(JsonNode.class);
+        JsonNode attribute_temp = mock(JsonNode.class);
+        JsonNode attribute_temp_son = mock(JsonNode.class);
+        JsonNode ports = mock(JsonNode.class);
+        JsonNode attributes = mock(JsonNode.class);
+        JsonNode portAttributes = mock(JsonNode.class);
+        JsonNode portAttribute = mock(JsonNode.class);
+        JsonNode attributes_son = mock(JsonNode.class);
+        JsonNode port = mock(JsonNode.class);
+        List<PhysicalNode> result;
+
+        when(nodesRoot.path(any(String.class))).thenReturn(nodes);
+        when(nodes.size()).thenReturn(1);
+        when(nodes.get(any(Integer.class))).thenReturn(node);
+        //get into method "buildnode"
+        when(node.get(any(String.class))).thenReturn(node_temp);
+        when(node_temp.asText())
+                .thenReturn(new String(""))//branch null
+                .thenReturn(new String("test")) //node_id
+                .thenReturn(new String("switch")); //NODE_TYPE
+
+
+        result = (List<PhysicalNode>) method.invoke(phyConfigLoader,nodesRoot); //return empty list
+        Assert.assertTrue(result.size() == 0);
+        verify(node_temp).asText();
+
+        when(node.path(any(String.class)))
+                .thenReturn(ports)//PORTS
+                .thenReturn(attributes);//ATTRIBUTES
+        ////get into method "buildports"
+        when(ports.size()).thenReturn(1);
+        when(ports.get(any(Integer.class))).thenReturn(port);
+        ///////get into method "buildport"
+        when(port.get(any(String.class))).thenReturn(port_temp);
+        when(port_temp.asText())
+                .thenReturn(new String("test"))//PORT_ID
+                .thenReturn(new String("switch"));//PORT_TYPE
+        when(port.path(any(String.class))).thenReturn(portAttributes);
+        ////////get into method "buildportattributes"
+        when(portAttributes.size()).thenReturn(1);
+        when(portAttributes.get(any(Integer.class))).thenReturn(attributes_son);
+        //////////get into method "buildPortAttribute"
+        when(attributes_son.path(any(String.class))).thenReturn(attribute_temp_son);
+        when(attribute_temp_son.asText())
+                .thenReturn(new String("zm"))
+                .thenReturn(new String("zm"));
+        ////return to method "buildnode" and get into method "..............buildNodeAttributes"
+        when(attributes.size()).thenReturn(1);
+        when(attributes.get(any(Integer.class))).thenReturn(portAttribute);
+        //////get into method "..............buildNodeAttribute"
+        when(portAttribute.path(any(String.class))).thenReturn(attribute_temp);
+        when(attribute_temp.asText())
+                .thenReturn(new String("test"))//ATTRIBUTE_NAME
+                .thenReturn(new String("test"));//ATTRIBUTE_VALUE
+
+        result = (List<PhysicalNode>) method.invoke(phyConfigLoader,nodesRoot); //return empty list
+        Assert.assertTrue(result.size() == 1);
+
+    }
+
 }
