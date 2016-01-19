65bf9408effa22ed798bded358a4cac18310b2f7
JieHou <houjie1@huawei.com>
2016-01-19 14:50:53 +0800

    Remove the dependency of cliche.
    Since cliche repository does not have maintaniners, remove the
    dependency of it and use another string-match way to implement
    this function.
    
    BugID 4995
    
    Change-Id: I81145b7e45151bd8dcb57aa334e20eed93cd00d6
    Signed-off-by: JieHou <houjie1@huawei.com>

--- nemo-tools/sandbox/src/main/java/org/opendaylight/nemo/tool/sandbox/Main.java
+++ nemo-tools/sandbox/src/main/java/org/opendaylight/nemo/tool/sandbox/Main.java
@@ -11,16 +11,43 @@ package org.opendaylight.nemo.tool.sandbox;
 import org.opendaylight.nemo.tool.sandbox.northbound.RestServer;
 import org.opendaylight.nemo.tool.sandbox.utils.PropertyLoader;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by hj on 12/9/15.
  */
 public class Main {
-    public static void main(String []args){
+    public static void main(String[] args) {
         PropertyLoader.loadProperties("/sandbox.properties", "sandbox.properties does not exist.");
-        int northboundPort = Integer.parseInt(System.getProperty("north.port","10081"));
-        String socketAddress = "http://0.0.0.0:"+northboundPort+"/";
+        int northboundPort = Integer.parseInt(System.getProperty("north.port", "10081"));
+        String socketAddress = "http://0.0.0.0:" + northboundPort + "/";
         RestServer.start(socketAddress);
-        EasyCli easyCli = new EasyCli("sandbox","Authorized by Ip tech, Huawei",new Cli());
-        easyCli.run();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.println("Enter command to create or destroy physical network (0 to exit):");
+            System.out.println("start/stop");
+            try {
+                String s = br.readLine();
+
+                if (s.equals("start")) {
+                    SandBoxManager.getInstance().createNetwork();
+                    continue;
+                } else if ("stop".equals(s)) {
+                    SandBoxManager.getInstance().destroyNetwork();
+                    continue;
+                }
+                int i = Integer.parseInt(s);
+                if (i == 0) {
+                    System.exit(0);
+                }
+            } catch (NumberFormatException nfe) {
+                System.err.println("Invalid Format!");
+            }catch (IOException io){
+                io.printStackTrace();
+            }
+        }
     }
 }
