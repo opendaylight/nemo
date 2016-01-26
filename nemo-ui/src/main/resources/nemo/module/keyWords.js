87565a0b7f9cda05b19ccf018c4d0c86a4b91635
swsiyu <swsiyu@163.com>
2016-01-26 19:10:07 +0800

    Bug 5108：The NEMO language is showed incompletely in NEMO UI
    -Change the keywords into uppercase character
    
    -Add condition
    
    -Add semicolon at the end of each sentence.
    
    
    Change-Id: Ib3d8515e3ace16f0afb3dda3128286ca1ac5db8f
    Signed-off-by: swsiyu <swsiyu@163.com>
    (cherry picked from commit 0f97f4072378392fdd29b97db351776a262dc2f6)

--- nemo-ui/src/main/resources/nemo/module/keyWords.js
+++ nemo-ui/src/main/resources/nemo/module/keyWords.js
@@ -1,1 +1,18 @@
-var keyWords=['Create','Import','Host','Node','Connection','Flow','Operation','Type','Endnodes','Property','Contain','Match','Target','Action'];
+var keyWords=['CREATE','IMPORT','Host','Node','Connection','Flow','Operation','Type','Endnodes','Property','Contain','Match','Target','Action','Condition'];
+var opChars={
+			"not-less-than":">=",
+			"less-than":"<",
+			"not-greater-than":"<=",
+			"greater-than":">",
+			"not-equal":"!=",
+			"equal":"==",
+			"between":"between",
+			"periodical":"periodical"
+			}
+var relChars={
+			"and":"&&",
+			"none":"",
+			"or":"||",
+			"not":"!",
+			}
+
