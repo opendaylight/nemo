f99e37a144c244ed9808373a803bd16657372712
Lorand Jakab <lojakab@cisco.com>
2016-06-29 15:47:49 -0500

    Upgrade ietf-{inet,yang}-types to 2013-07-15
    Change-Id: Ifc67395138834e414a26fb9cb329b3003f143862
    Signed-off-by: Lorand Jakab <lojakab@cisco.com>
    Signed-off-by: Hideyuki Tai <Hideyuki.Tai@necam.com>

--- nemo-renderers/openflow-renderer/src/main/java/org/opendaylight/nemo/renderer/openflow/physicalnetwork/PhysicalFlowUtils.java
+++ nemo-renderers/openflow-renderer/src/main/java/org/opendaylight/nemo/renderer/openflow/physicalnetwork/PhysicalFlowUtils.java
@@ -12,7 +12,7 @@ package org.opendaylight.nemo.renderer.openflow.physicalnetwork;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Uri;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.output.action._case.OutputAction;
